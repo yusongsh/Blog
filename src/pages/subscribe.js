d28ab6c3417f0563149a43077c83307d2d994aff
Yusong Shi <jackshi.sys@gmail.com>
2023-02-09 11:40:45 -0500

    form function

--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -1,13 +1,40 @@
 import React from "react";
 import Layout from "../components/layout";
 import Nav from "../components/Nav";
+import { navigate } from "gatsby";
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
+const handleSubmit = ({ name, ...restOfFormData }, event) => {
+  event.preventDefault();
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode({
+      "form-name": event.target.getAttribute("name"),
+      ...name,
+    }),
+  })
+    .then(() => console.log("/thank-you/"))
+    .catch((error) => alert(error));
+};
 
 export default function travelTips() {
   return (
     <>
       <Layout>
         <Nav />
-        <form name="contact" method="POST" data-netlify="true">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
           <p>
             <label>
               Your Name: <input type="text" name="name" />
